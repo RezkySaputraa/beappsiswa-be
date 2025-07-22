41005e9fbbd041301776047b1f8da351d30b5783
Muhammad Rezky Aulia Saputra <rezkyaulia907@gmail.com>
2025-07-22 12:27:38 +0800

    feat: deploy db to railway

--- src/api/beasiswa/routes.js
+++ src/api/beasiswa/routes.js
@@ -1,33 +1,37 @@
-import { PostBeasiswaPayloadSchema, PutBeasiswaPayloadSchema } from '../../validator/beasiswa/schema.js';
+import {
+  PostBeasiswaPayloadSchema,
+  PutBeasiswaPayloadSchema,
+} from "../../validator/beasiswa/schema.js";
 
 const routes = (handler) => [
   {
     method: "GET",
     path: "/api/beasiswa",
-    handler: handler.getScholarshipsHandler, 
+    handler: handler.getScholarshipsHandler,
   },
   {
     method: "GET",
     path: "/api/beasiswa/{id}",
-    handler: handler.getScholarshipByIdHandler, 
+    handler: handler.getScholarshipByIdHandler,
   },
   {
     method: "POST",
     path: "/api/beasiswa",
-    handler: handler.postBeasiswaHandler, 
+    handler: handler.postBeasiswaHandler,
     options: {
       validate: {
-        payload: PostBeasiswaPayloadSchema, 
+        payload: PostBeasiswaPayloadSchema,
       },
     },
   },
+  
   {
     method: "PUT",
     path: "/api/beasiswa/{id}",
-    handler: handler.putBeasiswaHandler, 
+    handler: handler.putBeasiswaHandler,
     options: {
       validate: {
-        payload: PutBeasiswaPayloadSchema, 
+        payload: PutBeasiswaPayloadSchema,
       },
     },
   },
@@ -39,7 +43,7 @@ const routes = (handler) => [
   {
     method: "GET",
     path: "/",
-    handler: handler.testingApi, 
+    handler: handler.testingApi,
   },
 ];
 
